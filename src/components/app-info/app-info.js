fab256b08d9b8e2cd55827276609907f1fcf5926
Serhii Odokiienko <58706778+odokienko-sergio@users.noreply.github.com>
2023-12-19 13:20:15 +0200

    Update app-info.js

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,9 +3,9 @@ import "./app-info.css";
 const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получат: {increased}</h2>
+            <h1>Employee accounting in company N</h1>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>The prize will be received by: {increased}</h2>
         </div>
     )
 }
